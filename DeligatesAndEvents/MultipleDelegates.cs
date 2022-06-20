647d330f9788759326a0c04432955b4a25d15228
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-20 20:07:42 +0530

    [vijayalakshmi] deligates single and multi values

---
+++ DeligatesAndEvents/MultipleDelegates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateandEvents
+{
+    public class MultipleDelegates
+    {
+        delegate void DelOp(int x, int y);
+        public void ImplementDelegate()
+        {
+            DelOp obj = Operation.Add;
+            obj += Operation.Square;
+            obj(6, 9);
+            obj(7, 8);
+            Console.ReadLine();
+        }
+        public class Operation
+        {
+            public static void Add(int a, int b)
+            {
+                Console.WriteLine("Addition is : {0}", a + b);
+            }
+            public static void Square(int a, int b)
+            {
+                Console.WriteLine("Multiple is : {0}", a * b);
+            }
+        }
+    }
+}
---
+++ DeligatesAndEvents/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DelegateandEvents
+{
+
+    // Delegate Definition or Declaration
+    public delegate int operation(int x, int y);
+    class program
+    {
+        // Method that is passes  as an Argument
+        // It has same signature as Delegates
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate of!");
+
+            //Deligate instantiation
+            operation obj = new operation(Addition);
+
+            //output
+            Console.WriteLine("Addition is={0}", obj(56, 78));
+            Console.WriteLine();
+            MultipleDelegates value = new MultipleDelegates();
+            value.ImplementDelegate();
+
+        }
+    }
+}
